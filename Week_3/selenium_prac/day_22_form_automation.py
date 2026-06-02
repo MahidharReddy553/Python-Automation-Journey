1388e9bdc1de3c11ee6b08318843a9870b5e2337
MahidharReddy553 <mahidharreddy158@gmail.com>
2026-06-02 18:18:45 +0530

    Added WebElement actions and textbox form automation

--- Week_3/selenium_prac/day_22_form_automation.py
+++ Week_3/selenium_prac/day_22_form_automation.py
@@ -2,25 +2,44 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+name = "Loki"
+email = "example@gmail.com"
+current_address = "TVA"
+permanent_address = "Asgard"
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://demoqa.com/text-box?utm_source=chatgpt.com")
 print('Title :', driver.title)
 time.sleep(1)
 
-driver.find_element(By.ID, "userName").send_keys("Loki")
+driver.find_element(By.ID, "userName").send_keys(name)
 time.sleep(1)
-driver.find_element(By.ID, "userEmail").send_keys("example@gmail.com")
+driver.find_element(By.ID, "userEmail").send_keys(email)
 time.sleep(1)
-driver.find_element(By.ID, "currentAddress").send_keys("TVA")
+driver.find_element(By.ID, "currentAddress").send_keys(current_address)
 time.sleep(1)
-driver.find_element(By.ID, "permanentAddress").send_keys("Asgard")
+driver.find_element(By.ID, "permanentAddress").send_keys(permanent_address)
 driver.execute_script('window.scrollTo(0,400);')
 time.sleep(1)
 driver.find_element(By.ID, "submit").click()
 time.sleep(1)
 
-output = driver.find_element(By.ID, "output")
+output_name = driver.find_element(By.ID, "name")
+output_email = driver.find_element(By.ID, "email")
+output_curr_address = driver.find_element(By.CSS_SELECTOR, "#currentAddress.mb-1")
+output_perm_address = driver.find_element(By.CSS_SELECTOR, "#permanentAddress.mb-1")
+
+def validate(actual, expected):
+    if expected.lower() in actual.lower():
+        print("Test passed")
+    else:
+        print("Test Failed")
+
+validate(output_name.text, name)
+validate(output_email.text, email)
+validate(output_curr_address.text, current_address)
+validate(output_perm_address.text, permanent_address)
+
 time.sleep(1)
-print(output.text)
 driver.quit()
