bcfd75cd379b411207922d75d97d5e61e496bddb
MahidharReddy553 <mahidharreddy158@gmail.com>
2026-06-27 22:36:15 +0530

    implemented conftest

--- Week_5/day_34/conftest.py
+++ Week_5/day_34/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from conftest import login_fixture, browser_fixture
 
 @pytest.fixture
 ## Browser fixture
