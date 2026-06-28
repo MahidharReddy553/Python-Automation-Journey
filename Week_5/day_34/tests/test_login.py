e7df65992f44ff252af1913347b9fbf285923a78
MahidharReddy553 <mahidharreddy158@gmail.com>
2026-06-28 10:50:32 +0530

    used fixtures from conftest

--- Week_5/day_34/tests/test_login.py
+++ Week_5/day_34/tests/test_login.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-@pytest.fixture
+
+# @pytest.fixture
 ## Browser fixture
 # def browser_fixture():
 #     driver = webdriver.Chrome()
