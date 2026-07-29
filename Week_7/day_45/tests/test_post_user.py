3cb1a29f7238860416d5cf8fc647e8ed988a1826
Mahidhar_Reddy <mahidharreddy158@gmail.com>
2026-07-29 15:39:02 +0530

    add: implement additional test for negative user creation scenario

--- Week_7/day_45/tests/test_post_user.py
+++ Week_7/day_45/tests/test_post_user.py
@@ -10,3 +10,14 @@ def test_add_user():
     response = post('users', payload = user_data)
 
     assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}"
+
+
+def test_negative_post_user():
+
+    user_data = {
+        "email" : 'invalid-email'
+    }
+
+    res = post('users/1', payload = user_data)
+    print(res.status_code)
+    assert res.status_code == 404, f"Expected status code 404, but got {res.status_code}"
