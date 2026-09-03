7796706fa1f5cf2ad3ec57b928555ad62d4647f7
Mahidhar_Reddy <mahidharreddy158@gmail.com>
2026-09-03 17:14:24 +0530

    add: implement query execution for fetching data from books table using python + MySQL

---
+++ SQL/Week_10/day_71/quuery_executions.py
@@ -0,0 +1,34 @@
+from mysql.connector import connect
+
+conn = connect(host = "localhost",
+               user = "root",
+               password = "root",
+               database = "mydb")
+
+
+cur = conn.cursor()
+
+query_1 = cur.execute("Select * from books;")
+q1_res = cur.fetchall()
+
+query_2 = cur.execute("Select * from books where price > %s;", ((500,)))
+q2_res = cur.fetchall()
+
+query_3 = cur.execute("Select * from books where price < %s;", ((500,)))
+q3_res = cur.fetchall()
+
+query_4 = cur.execute("Select * from books where price > %s;", ((400,)))
+q4_res = cur.fetchall()
+
+query_5 = cur.execute("Select * from books order by price desc;")
+q5_res = cur.fetchall()
+
+
+print("Query 1 result: ", q1_res)
+print("Query 2 result: ", q2_res)
+print("Query 3 result: ", q3_res)
+print("Query 4 result: ", q4_res)
+print("Query 5 result: ", q5_res)
+
+cur.close()
+conn.close()
